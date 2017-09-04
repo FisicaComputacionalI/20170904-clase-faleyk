7e954d589a2206736015aac604eebabe3c827df9
Fab <fab.cyl@gmail.com>
2017-09-04 13:37:45 -0500

    Update temp12.py

--- temp12.py
+++ temp12.py
@@ -3,4 +3,4 @@ import matplotlib.pylab as pl
 x=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
 y=[0,0,0,0,0,0,0,1,1,1,1,0,2,0,0,0,0,0,0,1,1,1]
 pl.plot(x,y)
-pl.savefig ('temp1.png')
+pl.savefig ('temp02.png')
